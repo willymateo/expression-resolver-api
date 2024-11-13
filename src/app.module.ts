277a73d217e7c5374f9b99f8dd60aa0cc6b2045a
willymateo <wjmateo@espol.edu.ec>
2024-11-13 18:55:49 -0500

    Add endpoint to resolve math expressions in string

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
+import { MathModule } from './math/math.module';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MathModule],
   controllers: [AppController],
   providers: [AppService],
 })
