13fc2b6c1905187736c93125e2afbdd979af9d8f
Willy Joao Mateo Espinoza <67135809+willymateo@users.noreply.github.com>
2024-11-14 00:32:04 -0500

    Add end-to-end test cases (#5)

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,7 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
+
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -12,13 +13,67 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/ [GET]', async () => {
+    const packageJson = await import('../package.json');
+
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect({
+        name: packageJson?.name,
+        author: packageJson?.author,
+        version: packageJson?.version,
+        description: packageJson?.description,
+        environment: process.env.NODE_ENV || 'development',
+      });
+  });
+
+  describe('/math/resolve [POST]', () => {
+    it('Succcess case', () => {
+      return request(app.getHttpServer())
+        .post('/math/resolve')
+        .send({
+          mathExpression: '10 * (2 + 5) * 10',
+        })
+        .expect(200)
+        .expect({
+          result: 700,
+        });
+    });
+
+    describe('Error cases', () => {
+      it('Bad request', () => {
+        return request(app.getHttpServer())
+          .post('/math/resolve')
+          .send({})
+          .expect(400)
+          .expect({
+            message: [
+              'mathExpression should not be empty',
+              'mathExpression must be a string',
+            ],
+            error: 'Bad Request',
+            statusCode: 400,
+          });
+      });
+
+      it('Unexpected token', () => {
+        return request(app.getHttpServer())
+          .post('/math/resolve')
+          .send({
+            mathExpression: '3ab6',
+          })
+          .expect(400)
+          .expect({
+            message: ["Unexpected token 'a'"],
+            error: 'Bad Request',
+            statusCode: 400,
+          });
+      });
+    });
   });
 });
